7969c3c138483c89e5eab1b3663308a086e1cf4f
Gejsi <vgejsi@gmail.com>
2022-12-25 21:59:17 +0100

    Initial commit

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,18 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'turbo',
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    'no-empty': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-};
+}
